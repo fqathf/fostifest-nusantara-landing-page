3d1f46a50659daea9f3173364c423ffb9d3869e6
fqathf <100032449+fqathf@users.noreply.github.com>
2023-10-24 19:02:44 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*const callback = function (entries) {
+    entries.forEach((entry) => {
+      console.log(entry);
+  
+      if (entry.isIntersecting) {
+        entry.target.classList.add("scale-in-center");
+      } 
+    });
+  };
+  
+  const observer = new IntersectionObserver(callback);
+  
+  const targets = document.querySelectorAll(".js-show-on-scroll");
+  targets.forEach(function (target) {
+    target.classList.add("opacity-0");
+    observer.observe(target);
+  });*/
+  
